dda3940917c9a40bf41236a5c386d7b58c9aaf9e
Ivan <ivaanpavlovic@gmail.com>
2024-12-07 15:29:55 +0100

    Final push

--- js/board-types.js
+++ js/board-types.js
@@ -11,7 +11,7 @@ function generateBoardTypeCards() {
   boards.forEach((board) => {
     // Create a Bootstrap card for each board
     const cardHTML = `
-    <a href="./board-products.html?size=${board.size}" class="col-12 col-sm-5 col-md-4 p-4 text-decoration-none" >
+    <a href="./board-products.html?size=${board.size}" class="col-12 col-sm-6 col-md-3 p-4 text-decoration-none" >
       <div class="card h-100 text-center"">
         <img class="card-img-top mt-2 bg-off-white" src="${board.image_link}" alt="${board.size} Board Image">
         <div class="card-body d-flex flex-row justify-content-center">
