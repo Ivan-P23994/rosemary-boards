3b69122969b8a92c2d6397fbd74464a576eb52c0
Ryan ONeill <ryanoneill211@gmail.com>
2024-12-07 14:24:28 +0000

    s

--- js/single-board-product.js
+++ js/single-board-product.js
@@ -1,6 +1,6 @@
 const boards = [
   { name: "Basil Board", price: "80 €", image_link: "./assets/medium_boards/medium-boards.jpg", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?" },
-  { name: "Tarragon Board", price: "80 €", image_link: "./assets/medium_boards/large-boards.jpg", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?" }
+  { name: "Tarragon Board", price: "80 €", image_link: "./assets/medium_boards/small-boards.jpg", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?" }
 ];
 
 
